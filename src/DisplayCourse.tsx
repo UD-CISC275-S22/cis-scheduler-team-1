c64c1cc6267e5ad131acbb8c486205121cd41175
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-04-09 14:04:11 -0400

    Working to display course with button

--- src/DisplayCourse.tsx
+++ src/DisplayCourse.tsx
@@ -1,16 +1,60 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 export function DisplayCourse(): JSX.Element {
-    const [course, setCourse] = useState<string>("Type course here");
+    const [Course, setCourse] = useState<string>(""); // current inputted course that was typed in
+    const [id, setID] = useState<string>(""); // course id that was typed in
+    const [courseList, setCourseList] = useState<string[]>([]); // a comprehensive course list for the semester
+    const [credits, setCredits] = useState<number>(0); // number of credits summed from taken courses
 
+    // updates the course that is being typed in
     function updateCourse(event: React.ChangeEvent<HTMLInputElement>) {
-        setCourse(event.target.value);
+        setCourse(event.target.value.toUpperCase());
+    }
+
+    // updates the id from text boz
+    function updateID(event: React.ChangeEvent<HTMLInputElement>) {
+        setID(event.target.value.toUpperCase());
+    }
+
+    // combines the course and id and adds the course to the course list, adds appropriate credits
+    function addCourse() {
+        const newCourse = Course + id;
+        if (!courseList.includes(newCourse)) {
+            setCourseList([...courseList, newCourse]);
+            setCredits(credits + 1);
+        }
+        setID("");
+        setCourse("");
     }
 
     return (
-        <Form.Group>
-            <Form.Control value={course} onChange={updateCourse}></Form.Control>
-        </Form.Group>
+        <div>
+            <strong>Courses: </strong>
+            {courseList.map((course: string) => (
+                <div key={course}>{course}</div>
+            ))}
+            <Container>
+                <Row>
+                    <Col>
+                        <Form.Control
+                            type="string"
+                            value={Course}
+                            onChange={updateCourse}
+                            placeholder="Type Department ID Here"
+                        ></Form.Control>
+                    </Col>
+                    <Col>
+                        <Form.Control
+                            type="number"
+                            value={id}
+                            onChange={updateID}
+                            placeholder="Type Course Number Here"
+                        ></Form.Control>
+                    </Col>
+                </Row>
+                <Button onClick={addCourse}>Add Course</Button>
+            </Container>
+        </div>
     );
 }
