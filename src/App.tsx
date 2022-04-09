ca3271b67ee58a11fcb5d4a919d64b358373f555
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-04-09 14:10:13 -0400

    adding semseter layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./App.css";
+import { SemesterLayout } from "./semesterLayout";
 
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Final Project
-            </header>
+            <header className="App-header">UD Course Scheduler</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
             Team 1: Jenn Werth and Disha Thakar
+            <SemesterLayout></SemesterLayout>
         </div>
     );
 }
