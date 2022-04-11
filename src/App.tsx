d826708cf9cc7698cbd8869459cf82ccb9cd7a1d
Jennifer Werth <jennwerth@wifi-roaming-128-4-158-246.wifi.udel.edu>
2022-04-11 15:54:35 -0400

    welcome message

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import { SemesterLayout } from "./semesterLayout";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">UD Course Scheduler</header>
+            <header className="App-header">
+                Welcome to UD Course Scheduler
+            </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                To add a new semester, click Add Semester. To add classes, enter
+                a valid course ID and click Add Course.
             </p>
             Team 1: Jenn Werth and Disha Thakar
             <SemesterLayout></SemesterLayout>
