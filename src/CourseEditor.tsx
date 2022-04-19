4ab3dac0364179fa466b2053b62aa9bdfcffc009
dthakar23 <dthakar@udel.edu>
2022-04-18 22:00:01 -0400

    edit component code

--- src/CourseEditor.tsx
+++ src/CourseEditor.tsx
@@ -1,17 +1,104 @@
-import React from "react";
-import { ListGroup, Form } from "react-bootstrap";
+import React, { useState } from "react";
+import { ListGroup, Form, Container, Col, Row, Button } from "react-bootstrap";
 import { course as Course } from "./interfaces/course";
 import coursedata from "./coursedata.json";
 
-interface CourseProps {
+/* interface CourseProps {
     course: Course;
     setCourse: (code: string, newCourse: Course) => void;
-}
+} */
 
-export function CourseCodeEditor({
-    course,
-    setCourse
-}: CourseProps): JSX.Element {
+export function CourseEditor(
+    {
+        changeEditing,
+        course,
+        editCourse
+    }: /*course,
+    setCourse */
+    {
+        changeEditing: () => void;
+        course: Course;
+        editCourse: (code: string, newCourse: Course) => void;
+    } /*CourseProps*/
+): JSX.Element {
+    const [code, setCode] = useState<string>(course.code);
+    const [name, setName] = useState<string>(course.name);
+    const [credits, setCredits] = useState<string>(course.credits);
+
+    function save() {
+        editCourse(course.code, {
+            ...course,
+            code: course.code,
+            name: course.name,
+            credits: course.credits
+        });
+        changeEditing();
+    }
+
+    function cancel() {
+        changeEditing();
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    {/* Course Code */}
+                    <Form.Group controlId="formCourseCode" as={Row}>
+                        <Form.Label column sm={2}>
+                            Code:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={course.code}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setCode(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/* Course Name */}
+                    <Form.Group controlId="formCourseName" as={Row}>
+                        <Form.Label column sm={2}>
+                            Name:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={course.name}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setName(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/* Course Credits */}
+                    <Form.Group controlId="formCourseCredits" as={Row}>
+                        <Form.Label column sm={2}>
+                            Credits:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={course.credits}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setCredits(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    {/* Save/Cancel */}
+                    <Button onClick={save} variant="success" className="me-4">
+                        Save
+                    </Button>
+                    <Button onClick={cancel} variant="warning" className="me-5">
+                        Cancel
+                    </Button>
+                </Col>
+            </Row>
+        </Container>
+    );
+
+    /*
+
     return (
         <Form.Control
             value={course.code}
@@ -73,8 +160,8 @@ export function CourseEditor({
                 course.code === code ? newCourse : course
             )
         );
-    }
-    return (
+    } */
+    /*return (
         <ListGroup as="ol" numbered>
             {courses.map((course: Course) => (
                 <ListGroup.Item
@@ -83,18 +170,18 @@ export function CourseEditor({
                     className="d-flex align-items-start"
                 >
                     <div className="ms-2 me-auto">
-                        {/* Course Code */}
-                        <CourseCodeEditor
+                        {/* Course Code */ //}
+    /*<CourseCodeEditor
                             course={course}
                             setCourse={setCourse}
                         ></CourseCodeEditor>
-                        {/* Course Name */}
-                        <CourseNameEditor
+                        {/* Course Name */ //}
+    /*<CourseNameEditor
                             course={course}
                             setCourse={setCourse}
                         ></CourseNameEditor>
-                        {/* Course Credits */}
-                        <CourseCreditsEditor
+                        {/* Course Credits */ //}
+    /*<CourseCreditsEditor
                             course={course}
                             setCourse={setCourse}
                         ></CourseCreditsEditor>
@@ -102,5 +189,5 @@ export function CourseEditor({
                 </ListGroup.Item>
             ))}
         </ListGroup>
-    );
+    );*/
 }
--- src/semesterLayout.tsx
+++ src/semesterLayout.tsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { DisplayCourse } from "./DisplayCourse";
+import { course as Course } from "./interfaces/course";
+import { semester as Semester } from "./interfaces/semester";
 
 export function SemesterLayout(): JSX.Element {
     const [semester, setSemester] = useState<string>(""); // current inputted semester
     const [semesterList, setSemesterList] = useState<string[]>([]); // store inputted semester into an array of semesters
+    const [semesterListReal, setSemesterListReal] = useState<Semester[]>([]);
+
+    /*function updateSemesterReal(id: number) {
+        setSemesterListReal(semesterListReal.target.value);
+    }*/
 
     function updateSemester(event: React.ChangeEvent<HTMLInputElement>) {
         setSemester(event.target.value);
     }
+
     // adds inputted semester to semester list, does not allow repeat semester names
     function addSemester() {
         if (!semesterList.includes(semester) && semester !== "") {
@@ -31,7 +39,9 @@ export function SemesterLayout(): JSX.Element {
                     <div key={semester}>
                         <h4>{semester}</h4>
                     </div>
-                    <DisplayCourse></DisplayCourse>
+                    {/*there should be a map for semester here where semester is an interface not string[]*/}
+                    <DisplayCourse /*course={course}*/></DisplayCourse>
+
                     <Button onClick={() => deleteSemester(semester)}>
                         Delete Semester
                     </Button>
