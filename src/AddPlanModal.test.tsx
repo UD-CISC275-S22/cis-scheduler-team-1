e9ea3a4eb2863e2101f31ef898c7a9c4f89be139
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-04-18 21:47:03 -0400

    linter errors

--- src/AddPlanModal.test.tsx
+++ src/AddPlanModal.test.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-    getAllByText,
-    getByText,
-    render,
-    screen
-} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
