9896db2e661e829261858e352833039ee7cbd81c
Jennifer Werth <jennwerth@jennifers-mbp-2.myfiosgateway.com>
2022-04-21 10:28:31 -0400

    organizing components into folder

---
+++ src/components/CourseEditor.tsx
@@ -0,0 +1,142 @@
+import React, { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { course } from "../interfaces/course";
+
+export function CourseEditor({
+    changeEditing,
+    course,
+    editCourse
+}: {
+    changeEditing: () => void;
+    course: course;
+    editCourse: (code: string, newCourse: course) => void;
+}): JSX.Element {
+    // create state variables for every attribute in course
+    const [name, setName] = useState<string>(course.name); // original name for course
+    const [credits, setCredits] = useState<string>(course.credits);
+    const [description, setDescription] = useState<string>(course.descr);
+    const [preReq, setPreReq] = useState<string>(course.preReq);
+    const [restrict, setRestrict] = useState<string>(course.restrict);
+    const [breadth, setBreadth] = useState<string>(course.breadth);
+    const [type, setType] = useState<string>(course.typ);
+
+    function saveChanges() {
+        editCourse(course.code, {
+            ...course,
+            name: name,
+            credits: credits,
+            descr: description,
+            preReq: preReq,
+            restrict: restrict,
+            breadth: breadth,
+            typ: type
+        });
+        changeEditing();
+    }
+
+    function close() {
+        changeEditing(); // close without saving
+    }
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Form.Group controlId="courseName" as={Row}>
+                        <Form.Label column sm={3}>
+                            Course Name:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={name}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setName(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="courseDescr" as={Row}>
+                        <Form.Label column sm={3}>
+                            Course Description:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={description}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setDescription(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="courseCredits" as={Row}>
+                        <Form.Label column sm={3}>
+                            Course Credits:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={credits}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setCredits(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="coursePreReqs" as={Row}>
+                        <Form.Label column sm={3}>
+                            Course PreRequisites:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={preReq}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setPreReq(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="courseRestrict" as={Row}>
+                        <Form.Label column sm={3}>
+                            Course Restrictions:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={restrict}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setRestrict(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="courseBreadth" as={Row}>
+                        <Form.Label column sm={3}>
+                            Course Breadth Fulfillments:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={breadth}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setBreadth(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                    <Form.Group controlId="courseType" as={Row}>
+                        <Form.Label column sm={3}>
+                            Course Offered In:
+                        </Form.Label>
+                        <Col>
+                            <Form.Control
+                                value={type}
+                                onChange={(
+                                    event: React.ChangeEvent<HTMLInputElement>
+                                ) => setType(event.target.value)}
+                            />
+                        </Col>
+                    </Form.Group>
+                </Col>
+                <Button onClick={saveChanges}>Save Changes</Button>
+                <Button onClick={close}>Cancel</Button>
+            </Row>
+        </Container>
+    );
+}
---
+++ src/components/SemesterLayout.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import { DisplayCourse } from "./DisplayCourse";
+
+export function SemesterLayout(): JSX.Element {
+    const [semester, setSemester] = useState<string>(""); // current inputted semester
+    const [semesterList, setSemesterList] = useState<string[]>([]); // store inputted semester into an array of semesters
+
+    function updateSemester(event: React.ChangeEvent<HTMLInputElement>) {
+        setSemester(event.target.value);
+    }
+
+    // adds inputted semester to semester list, does not allow repeat semester names
+    function addSemester() {
+        if (!semesterList.includes(semester) && semester !== "") {
+            setSemesterList([...semesterList, semester]);
+        }
+        setSemester("");
+    }
+
+    function deleteSemester(semester: string) {
+        const updatedList = [...semesterList];
+        const index = updatedList.indexOf(semester);
+        updatedList.splice(index, 1);
+        setSemesterList(updatedList);
+    }
+
+    function clearSemesters() {
+        setSemesterList([]);
+    }
+
+    return (
+        <div>
+            <div className="bg-white border m-2 p-2">
+                {semesterList.map((semester: string) => (
+                    <Container key={semester}>
+                        <div key={semester}>
+                            <h4>{semester}</h4>
+                        </div>
+                        <DisplayCourse></DisplayCourse>
+
+                        <Button onClick={() => deleteSemester(semester)}>
+                            Delete Semester
+                        </Button>
+                        <hr></hr>
+                    </Container>
+                ))}
+            </div>
+            <Form.Group>
+                <Form.Control
+                    value={semester}
+                    onChange={updateSemester}
+                    placeholder="Type semester here"
+                ></Form.Control>
+            </Form.Group>
+            <Button onClick={addSemester}>Add New Semester</Button>
+            <Button onClick={clearSemesters}>Clear Semesters</Button>
+        </div>
+    );
+}
