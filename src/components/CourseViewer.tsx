8964ffd39182fa308b591e5a81b087d841003c65
Jennifer Werth <jennwerth@wifi-roaming-128-4-78-108.wifi.udel.edu>
2022-04-25 10:36:20 -0400

    Separating components to avoid mapping issues

---
+++ src/components/CourseViewer.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { CourseEditor } from "./CourseEditor";
+import { course as Course } from "../interfaces/course";
+import { Button, Container } from "react-bootstrap";
+
+export function CourseViewer({
+    course,
+    editCourse
+}: {
+    course: Course;
+    editCourse: (code: string, newCourse: Course) => void;
+}): JSX.Element {
+    const [editing, setEditing] = useState<boolean>(false);
+
+    function changeEditing() {
+        setEditing(!editing);
+    }
+
+    return editing ? (
+        <CourseEditor
+            changeEditing={changeEditing}
+            course={course}
+            editCourse={editCourse}
+        ></CourseEditor>
+    ) : (
+        <Container
+            key={course.code}
+            style={{ border: "1px solid white", padding: "6px" }}
+        >
+            <div
+                style={{
+                    border: "1px solid black",
+                    padding: "6px"
+                }}
+            >
+                <h6>
+                    {course.code}: {course.name}
+                </h6>
+                <p>
+                    {course.descr}
+                    <div>
+                        {" "}
+                        <p>This class is worth {course.credits} credits</p>
+                    </div>
+                </p>
+            </div>
+            <Button onClick={changeEditing}>Edit Course</Button>
+        </Container>
+    );
+}
