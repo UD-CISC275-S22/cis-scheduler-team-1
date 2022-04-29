87f982a75030decf6b8b852c52189cd45b5229d2
Jennifer Werth <jennwerth@wifi-roaming-128-4-123-217.wifi.udel.edu>
2022-04-28 22:31:22 -0400

    adding requirement info

--- src/components/RequirementView.tsx
+++ src/components/RequirementView.tsx
@@ -7,6 +7,11 @@ export function RequirementView({ plan }: { plan: Degreeplan }): JSX.Element {
         <div className="bg-white border m-2 p-2">
             <Container>
                 <h4>Degree Fulfillments</h4>
+                <p>
+                    In order to graduate with a bachelors degree you must
+                    complete 120 credits. Complete 2 semesters a year to
+                    graduate in 4 years.
+                </p>
                 <Row>Total Semesters: {plan.semesters.length}</Row>
                 <Row>Total Credits: {plan.totalCredits} / 120</Row>
             </Container>
